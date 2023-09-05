1f13a62cd7e0a43baa78ce3d5f53182e20212e37
muendokennedy <kennedymunyao999@gmail.com>
2023-09-05 09:44:08 +0300

    Added the mobile menu toggling functionality using the javascript

--- build/js/script.js
+++ build/js/script.js
@@ -7,3 +7,28 @@ accordion.forEach(accordion => {
     accordion.querySelector('.accordion i').classList.toggle('fa-plus');
   }
 }); 
+
+
+// Making the mobile menu work
+
+const menubarBtn = document.getElementById('humbuger-btn');
+
+const mobileMenu = document.querySelector('.mobile-menu-navigation-bar');
+
+const mobileMenuToggler = document.querySelector('.menu-toggle');
+
+
+
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('active');
+  menubarBtn.classList.toggle('fa-xmark');
+}
+// Toggling the mobile menu navigation bar by click the icon bar icon at the top
+menubarBtn.onclick = () => {
+  toggleMobileMenu();
+}
+
+// Optionally toggling the mobile menu by clicking outside the naviagation bar
+mobileMenuToggler.onclick = () => {
+  toggleMobileMenu();
+}
