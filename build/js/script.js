d899ff61b4f831048393cfb063010cf29c5bd4e3
muendokennedy <kennedymunyao999@gmail.com>
2023-09-06 10:29:54 +0300

    Added a swiper with pagination in the about page as well as made the whole page mobile responsive

--- build/js/script.js
+++ build/js/script.js
@@ -40,4 +40,29 @@ mobileFooterToggler.onclick = () => {
   mobileMenu.classList.remove('active');
   menubarBtn.classList.remove('fa-xmark');
 }
+// Working of the swiper slide section of the reviews  section
+const clientReviewsSwiper = new Swiper(".client-reviews-container", {
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      }
+    }
+});
 
