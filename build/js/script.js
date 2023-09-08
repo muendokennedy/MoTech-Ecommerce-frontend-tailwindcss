263600ac4da06f7b30529e0cbbba33606d20113d
muendokennedy <kennedymunyao999@gmail.com>
2023-09-08 08:50:52 +0300

    Added the custom js code to its own file to prevent the console errors on other pages

--- build/js/script.js
+++ build/js/script.js
@@ -40,29 +40,5 @@ mobileFooterToggler.onclick = () => {
   mobileMenu.classList.remove('active');
   menubarBtn.classList.remove('fa-xmark');
 }
-// Working of the swiper slide section of the reviews  section
-const clientReviewsSwiper = new Swiper(".client-reviews-container", {
-  centeredSlides: true,
-  spaceBetween: 20,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      450: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      }
-    }
-});
+
 
---
+++ build/js/swiperSider.js
@@ -0,0 +1,25 @@
+// Working of the swiper slide section of the reviews  section
+const clientReviewsSwiper = new Swiper(".client-reviews-container", {
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      }
+    }
+});
